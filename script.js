c63ff6235c78540b5cd0b89ba087040b11826ddd
dorjeezzz <dj.namjyal@gmail.com>
2024-07-08 14:24:35 -0600

    Implemented basic calculator functions and operation/number buttons

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(x, y){
+    return x+y;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+
+function multiply(x,y){
+    return x*y;
+}
+
+function divide(x,y){
+    return x/y;
+}
+let num1;
+let num2;
+let operator;
+
+function operate(num1, operator, num2){
+    if (operator == '+'){
+        add(num1, num2);
+    }
+    else if (operator == '-'){
+        subtract(num1,num2);
+    }
+    else if (operator == '*'){
+        multiply(num1,num2);
+    }
+    else divide(num1,num2);
+}
+
