bcf029705eb057e8d4cbc9361ead7979ae4bbd5e
dorjeezzz <dj.namjyal@gmail.com>
2024-07-15 18:00:27 -0600

    Finished final case for division when clicks == 1

--- script.js
+++ script.js
@@ -32,9 +32,6 @@ function operate(num1, operator, num2){
     else return null;
 }
 
-const sel = "grey";
-const col = "black";
-
 const buttons = document.querySelectorAll(".button");
 //everytime click button put value on display then store it somewhere so you can use operation func on it, count number of buttons pressed
 //needs to == 3 in order to call operation function, if alr number on view count -= 2;
@@ -43,7 +40,6 @@ let a = 0;
 let b;
 let ops;
 let clicks = 0;
-let twonums = false;
 let it = 0;
 buttons.forEach((button)=>{
     let val = button.textContent;
@@ -72,15 +68,12 @@ buttons.forEach((button)=>{
                 let n = Number(display.textContent);
                 ops = '*';
                 a = n;
-                // console.log(`a = ${a}`);
-                // console.log(ops);
                 clicks++;
             }
             else if (val == '/'){
                 let n = Number(display.textContent);
                 ops = '/';
                 a = n;
-                //else pressed = false;
                 clicks++;
             }
             else if (val == '+'){
@@ -151,6 +144,25 @@ buttons.forEach((button)=>{
                     b = 0;
                     a = Number(display.textContent);
                     ops = '-';
+                    it = 0;
+                }
+                else if (ops != "" && it > 0){
+                    b = n;
+                    display.textContent = operate(a,ops,b);
+                    b = 0;
+                    a = Number(display.textContent);
+                    it = 0;
+                }
+            }
+            else if (val == '/'){
+                let n = Number(display.textContent);
+                if (ops == "") ops = '/';
+                else if (ops != "" && ops != '/'){
+                    b = n;
+                    display.textContent = operate(a,ops,b);
+                    b = 0;
+                    a = Number(display.textContent);
+                    ops = '/';
                     it = 0;
                 }
                 else if (ops != "" && it > 0){
